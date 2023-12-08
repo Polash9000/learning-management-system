5c23116d08e0850d4bcbc7f8af84b158f4241aef
Polash9000 <85825314+Polash9000@users.noreply.github.com>
2023-12-08 22:53:51 +0600

    added

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Compass, Layout } from 'lucide-react';
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: 'Dashboard',
+    href: '/',
+  },
+  {
+    icon: Compass,
+    label: 'Browse',
+    href: '/search',
+  },
+];
+
+export const SidebarRoutes = () => {
+  const routes = guestRoutes;
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem icon={route.href} label={route.label} />
+      ))}
+    </div>
+  );
+};
