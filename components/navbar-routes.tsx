2e1a7bb34e2d278a6f43c977243a4357b1ebf6ca
Polash9000 <85825314+Polash9000@users.noreply.github.com>
2023-12-10 13:10:08 +0600

    create page and form added

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { UserButton } from '@clerk/nextjs';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { LogOut } from 'lucide-react';
 import Link from 'next/link';
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith('/teacher');
   const isPlayerPage = pathname?.includes('/chapter');
@@ -17,16 +16,21 @@ export const NavbarRoutes = () => {
     <div className="flex gap-x-2 ml-auto">
       <div className="flex gap-x-2 ml-auto">
         {isTeacherPage || isPlayerPage ? (
-          <Button>
-            <LogOut className="h-4 w-4 mr-2" />
-          </Button>
+          <Link href="/">
+            <Button>
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
         ) : (
           <Link href="/teacher/courses">
-            <Button>Teacher mode</Button>
+            <Button size="sm" variant="ghost">
+              Teacher mode
+            </Button>
           </Link>
         )}
       </div>
-      <UserButton />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
